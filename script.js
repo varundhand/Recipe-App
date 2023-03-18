9baa5c9ee5d795d21dfc0048ff37ef4d62642139
varundhand <varundhand77@gmail.com>
2023-03-18 16:55:00 +0530

    Checkpoint

--- script.js
+++ script.js
@@ -11,26 +11,35 @@ const getRandomMeal = async () => {
 
   addMeal(randomMeal, true);
 };
+// Usually when an async function is called, we use await.
+// But, since we are assignig it to a new variable. We skipped `await`
 getRandomMeal();
+fetchFavMeals();
 
-const getMealById = async (id) => {
-  const meal = await fetch(
-    "www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+async function getMealById(id) {
+  console.log({ id });
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
   );
-};
+  const respData = await resp.json();
+  console.log(respData);
+  const meal = respData.meals[0];
+  return meal;
+}
 
 const getMealBySearch = async (term) => {
   const meals = await fetch(
-    "www.themealdb.com/api/json/v1/1/search.php?s=" + term
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
   );
 };
 
-const sliced = (text) => {
-  const finalText = text.split(" ").slice(0, 90).join(" ");
+const sliced = (text, words) => {
+  const finalText = text.split(" ").slice(0, words).join(" ");
   return finalText + "...";
 };
 
 const addMeal = (mealData, random = false) => {
+  console.log("here =>", mealData);
   // default value of random is false
   const meal = document.createElement("div");
   meal.classList.add("meal");
@@ -56,7 +65,7 @@ const addMeal = (mealData, random = false) => {
       <li>${mealData.strIngredient3}</li>
       <li>${mealData.strIngredient4}</li>
     </ul>
-    <p class="meal-body-des">${sliced(mealData.strInstructions)}</p>
+    <p class="meal-body-des">${sliced(mealData.strInstructions, 90)}</p>
   </div>
   `;
 
@@ -73,24 +82,62 @@ const addMeal = (mealData, random = false) => {
     }
     LikeButton.classList.toggle("fa-regular");
     LikeButton.classList.toggle("fa-solid");
-
-    // addMealToLS(meal)
+    // console.log("me", { mealData });
+    // const mealId = mealData.idMeal;
+    // // Update UI
+    // addMealToLS(mealId);
   });
 };
 
+function getMealsFromLS() {
+  const mealids = JSON.parse(localStorage.getItem("mealids"));
+  console.log(mealids);
+  return mealids === null ? [] : mealids;
+}
+
 const addMealToLS = (addedId) => {
-  const mealids = gteMealsFromLS();
+  const mealids = getMealsFromLS();
 
   localStorage.setItem("mealids", JSON.stringify([...mealids, addedId])); // [...] is the spread operator which adds individual items of an array
 };
+
 const removeMealsFromLS = (removedId) => {
-  const mealids = gteMealsFromLS();
+  const mealids = getMealsFromLS();
   localStorage.setItem(
     "mealids",
     JSON.stringify(mealids.filter((id) => id !== removedId))
   );
 };
-const gteMealsFromLS = () => {
-  const mealids = JSON.parse(localStorage.getItem("mealids"));
-  return mealids === null ? [] : mealids;
-};
+
+//! Gets all the favourite meals to be rendered at top
+async function fetchFavMeals() {
+  const mealids = getMealsFromLS();
+
+  const meals = [];
+
+  for (let i = 0; i < mealids.length; i++) {
+    // we cant use forEach method because we need to await each one of them
+    const mealid = mealids[i];
+    const meal = await getMealById(mealid); // since it returns a promise we need to await it
+    console.log(meal);
+    meals.push(meal);
+  }
+
+  // Add to UI
+  //! my way
+
+  meals.forEach((meal, index) => {
+    const liTag = document.createElement("li");
+    console.log(meal);
+    liTag.innerHTML = `
+    <img src="${meal.strMealThumb}" alt=""><span>${meal.strMeal}</span>
+    `;
+
+    const ulGetter = document.querySelector(".fav-meals");
+    ulGetter.append(liTag);
+  });
+}
+
+// [1,2,3]
+
+// fetch (meal = 3)
